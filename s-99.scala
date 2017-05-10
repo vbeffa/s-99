0f4639bd97af6f753efbaecf4567ac0404eb33df
Vlad Beffa <vbeffa@users.noreply.github.com>
2017-05-09 20:17:04 -0700

    Update s-99.scala

--- s-99.scala
+++ s-99.scala
@@ -4,3 +4,29 @@ def last[A](ls: List[A]): A = ls match {
   case _ :: tail => last(tail)
   case _ => throw new NoSuchElementException
 }
+
+// P02
+def penultimate[A](ls: List[A]): A = ls match {
+  case penultimate :: ultimate :: Nil => penultimate
+  case _ :: tail => penultimate(tail)
+  case _ => throw new NoSuchElementException
+}
+
+// P03
+def nth[A](i: Int, ls: List[A]): A = i match {
+  case x if x < 0 => throw new IllegalArgumentException("i must be non-negative")
+  case 0 => ls.head
+  case i => nth(i - 1, ls.tail)
+}
+
+// P04
+def length[A](ls: List[A]): Int = ls match {
+  case Nil => 0
+  case _ :: tail => 1 + length(tail)
+}
+
+// P05
+def reverse[A](ls: List[A]): List[A] = ls match {
+  case Nil => Nil
+  case head :: tail => reverse(tail) :+ head
+}
